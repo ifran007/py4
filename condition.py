d05a4ab2b7d6cd1c2d55a1dfb3751fbf00c9d457
Ifran <tifran007@gmail.com>
2021-09-29 00:08:22 +0600

    second

---
+++ condition.py
@@ -0,0 +1,14 @@
+x = 5
+if x < 10:
+    print("Your number is smaller")
+    print("Your number is still smaller")
+
+if x > 20:
+    print("Your number is bigger")
+
+for i in range(5):
+    print(i)
+    if i > 2:
+        print("Bigger than 2...::")
+    print("Done with i = ", i)
+print("concluded")
